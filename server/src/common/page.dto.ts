ecaedcf6666c32caa73f9eec57bf428d99cc7fc5
aklry <3265242862@qq.com>
2024-12-09 14:35:51 +0800

    feat(参数校验): 使用管道技术添加参数的校验

--- server/src/common/page.dto.ts
+++ server/src/common/page.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsOptional, IsString } from 'class-validator'
 
 export class PageRequest {
     @ApiProperty({ description: '当前页' })
-    current: number
+    @IsString()
+    current: string
     @ApiProperty({ description: '每页条数' })
-    pageSize: number
-    @ApiProperty({ description: '排序字段' })
+    @IsString()
+    pageSize: string
+    @ApiProperty({ description: '排序字段', required: false })
+    @IsOptional()
+    @IsString()
     sort?: string
-    @ApiProperty({ description: '排序方式' })
+    @ApiProperty({ description: '排序方式', required: false })
+    @IsOptional()
+    @IsString()
     sortOrder?: string
 }
--- server/src/config/response.ts
+++ server/src/config/response.ts
@@ -2,6 +2,7 @@
 export const BusinessStatus = {
     SUCCESS: { code: 1, message: '操作成功' },
     PARAMS_ERROR: { code: 40000, message: '请求参数错误' },
+    PARAMS_VALIDATION_ERROR: { code: 40001, message: '请求参数验证失败' },
     NOT_LOGIN_ERROR: { code: 40100, message: '未登录' },
     NOT_AUTH_ERROR: { code: 40101, message: '无权限' },
     NOT_FOUND_ERROR: { code: 40400, message: '请求数据不存在' },
