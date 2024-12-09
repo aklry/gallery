639a8d21e3bba467c6c6226bb252bc23ef23d370
aklry <3265242862@qq.com>
2024-12-09 16:09:11 +0800

    feat(exception): 添加权限守卫以及自定义异常

--- server/src/filters/exception.ts
+++ server/src/filters/exception.ts
@@ -1,6 +1,5 @@
-import { ArgumentsHost, Catch, ExceptionFilter, ForbiddenException, HttpException } from '@nestjs/common'
-import { BusinessStatus } from '../config'
-
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common'
+import { NotLoginException } from '../custom-exception'
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
@@ -16,17 +15,17 @@ export class HttpExceptionFilter implements ExceptionFilter {
     }
 }
 
-@Catch(ForbiddenException)
-export class ForbiddenExceptionFilter implements ExceptionFilter {
-    catch(exception: ForbiddenException, host: ArgumentsHost) {
+@Catch(NotLoginException)
+export class NotLoginExceptionFilter implements ExceptionFilter {
+    catch(exception: NotLoginException, host: ArgumentsHost) {
         const ctx = host.switchToHttp()
         const response = ctx.getResponse()
         const request = ctx.getRequest()
-        response.status(exception.getStatus()).json({
+        response.status(401).json({
             timestamp: new Date().toISOString(),
             path: request.url,
-            message: BusinessStatus.FORBIDDEN_ERROR.message,
-            code: BusinessStatus.FORBIDDEN_ERROR.code
+            message: exception.message,
+            code: exception.getStatus()
         })
     }
 }
