90c80c8e95838c78db1fc4182942303975f752f3
aklry <3265242862@qq.com>
2025-02-19 17:37:47 +0800

    build(vite): vite配置打包优化

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'node:url'
+import ViteCompression from 'vite-plugin-compression'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import globalExternal from 'rollup-plugin-external-globals'
 
+const globals = globalExternal({
+    dayjs: 'dayjs',
+    echarts: 'echarts',
+    lodash: 'lodash',
+    vueEcharts: 'vueEcharts',
+    echartsWordcloud: 'echartsWordcloud'
+})
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        ViteCompression({
+            threshold: 50 * 1024,
+            algorithm: 'brotliCompress'
+        }),
+        createHtmlPlugin({
+            inject: {
+                data: {
+                    title: '源空间',
+                    dayScript: `<script src='https://cdn.bootcdn.net/ajax/libs/dayjs/1.11.11/dayjs.min.js' async></script>`,
+                    lodashScript: `<script src='https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.21/lodash.min.js' async></script>`,
+                    echartsScript: `<script src='https://cdn.bootcdn.net/ajax/libs/echarts/5.6.0/echarts.min.js' async></script>`,
+                    vueEchartsScript: `<script src="https://cdn.bootcdn.net/ajax/libs/vue-echarts/7.0.3/index.min.js"></script>`,
+                    echartsWordcloudScript: `<script src="https://cdn.bootcdn.net/ajax/libs/echarts-wordcloud/2.1.0/echarts-wordcloud.min.js"></script>`
+                }
+            }
+        })
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -23,6 +51,28 @@ export default defineConfig({
             scss: {
                 additionalData: `@use "@/assets/styles/mixins/index" as mixins;`
             }
+        }
+    },
+    build: {
+        target: 'esnext',
+        outDir: 'dist',
+        minify: 'terser',
+        cssCodeSplit: true,
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'js/[name]-[hash].js',
+                assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+                entryFileNames: 'js/entry-[hash].js',
+                manualChunks: (id: string) => {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString()
+                    }
+                    return 'vendor'
+                }
+            },
+            external: ['dayjs', 'echarts', 'lodash'],
+            plugins: [globals]
         }
     }
 })
