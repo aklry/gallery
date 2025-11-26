39f42a0e5eef8a3240127a08a935192448edeb69
aklry <3265242862@qq.com>
2025-11-26 21:14:10 +0800

    refactor(prisma): 去除已废弃的api

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -58,42 +58,47 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
         this.$on('error' as never, (e: any) => {
             throw new BusinessException(BusinessStatus.SYSTEM_ERROR.message, BusinessStatus.SYSTEM_ERROR.code)
         })
-        // 添加中间件处理软删除
-        this.$use(async (params, next) => {
-            // 查询操作添加 isDelete = 0 条件
-            if (params.action === 'findUnique' || params.action === 'findFirst') {
-                // 改为 findFirst 操作
-                params.action = 'findFirst'
-                // 添加 isDelete 条件
-                params.args.where = { ...params.args.where, isDelete: 0 }
-            }
 
-            if (params.action === 'findMany') {
-                // 添加 isDelete 条件
-                if (params.args.where && 'isDelete' in params.args.where) {
-                    return next(params)
+        // 使用 Prisma Client Extensions 替代 $use
+        const extended = this.$extends({
+            query: {
+                $allModels: {
+                    async findUnique({ args, query }) {
+                        args.where = { ...args.where, isDelete: 0 }
+                        return query(args)
+                    },
+                    async findFirst({ args, query }) {
+                        args.where = { ...args.where, isDelete: 0 }
+                        return query(args)
+                    },
+                    async findMany({ args, query }) {
+                        if (args.where && 'isDelete' in args.where) {
+                            return query(args)
+                        }
+                        args.where = { ...args.where, isDelete: 0 }
+                        return query(args)
+                    },
+                    async count({ args, query }) {
+                        args.where = { ...args.where, isDelete: 0 }
+                        return query(args)
+                    },
+                    async delete({ model, args }) {
+                        // 转换为 update 操作
+                        return (this as any)[model].update({
+                            ...args,
+                            data: { isDelete: 1 }
+                        })
+                    },
+                    async deleteMany({ model, args }) {
+                        // 转换为 updateMany 操作
+                        return (this as any)[model].updateMany({
+                            ...args,
+                            data: { isDelete: 1 }
+                        })
+                    }
                 }
-                params.args.where = { ...params.args.where, isDelete: 0 }
             }
-
-            if (params.action === 'count') {
-                params.args.where = { ...params.args.where, isDelete: 0 }
-            }
-
-            // 删除操作改为更新 isDelete = 1
-            if (params.action === 'delete') {
-                // 转换为 update 操作
-                params.action = 'update'
-                params.args['data'] = { isDelete: 1 }
-            }
-
-            if (params.action === 'deleteMany') {
-                // 转换为 updateMany 操作
-                params.action = 'updateMany'
-                params.args['data'] = { isDelete: 1 }
-            }
-
-            return next(params)
         })
+        return Object.assign(this, extended)
     }
 }
